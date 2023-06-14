71001031e4028b3538bb1b80c771f1903e730a3e
mini <hanbv@naver.com>
2023-06-14 17:24:49 +0900

    -

--- Day06/mqtt_controller.py
+++ Day06/mqtt_controller.py
@@ -6,12 +6,24 @@ from threading import Thread, Timer
 import time
 import json
 import datetime as dt
-
+import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
 import Adafruit_DHT as dht
 
+# GPIO, DHT 설정
 sensor = dht.DHT11
 rcv_pin = 10
+green = 22
+servo_pin = 18
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
+GPIO.setup(green, GPIO.OUT)
+GPIO.output(green, GPIO.HIGH)
+
+pwm = GPIO.PWM(servo_pin, 50)
+pwm.start(3)
 
 class publisher(Thread):
     def __init__(self):
@@ -36,9 +48,36 @@ class publisher(Thread):
         Timer(2.0, self.publish_data_auto).start() # 2초마다 출판
 
 class subscriber(Thread):
-    pass
+    def __init__(self):
+        Thread.__init__(self)
+        self.host = '210.119.12.67'
+        self.port = 1883
+        self.clientId = 'IOT67_SUB'
+        self.topic = 'pknu/monitor/control'
+        print('subscrib 스레드 시작')
+        self.client = mqtt.Client(client_id=self.clientId)
 
+    def run(self):
+        self.client.on_connect = self.onConnect # 접속 성공 시그널 처리
+        self.client.on_message = self.onMessage # 접속 후 메세지 수신되면 처리
+        self.client.connect(self.host, self.port)
+        self.client.subscribe(topic=self.topic)
+        self.client.loop_forever()
+
+    def onConnect(self, mqttc, obj, flags, rc):
+        print(f'subscriber 연결됨 rc > {rc}')
+
+    def onMessage(self, mqttc, obj, msg):
+        rcv_msg = str(msg.payload.decode('utf-8'))
+        print(f'{msg.topic} / {rcv_msg}')
+        data = json.loads(rcv_msg)
+        stat = data['STAT']
+        print(f'현재 STAT : {stat}')
+        time.sleep(1.0)
+        if(stat == )
+ 
 if __name__ == '__main__':
     thPub = publisher()
     thSub = subscriber()
     thPub.start() # run()이 자동으로 실행
+    thSub.start()
